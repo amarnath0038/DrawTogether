a0e7d520d72b2588a2872e61e6a9d1fb51a5a741
Amarnath <amarnathreddy841@gmail.com>
2025-05-17 21:02:24 +0530

    Made landing page and some other minor changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,41 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.com/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div 
+      className="min-h-screen w-screen px-4 flex flex-col"
+      style={{
+        backgroundImage: `url('/Bg1.jpeg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
+    
+      <nav className="mx-auto mt-6 w-[95%] max-w-7xl bg-black/60 backdrop-blur-md shadow-lg rounded-xl px-6 py-4 flex justify-between items-center">
+        <div className="text-2xl font-bold text-white">Kind Of Draw</div>
+        <div className="space-x-4">
+          <button className="text-gray-200 hover:text-white hover:font-semibold font-medium">Sign In</button>
+          <button className="bg-indigo-800 text-white px-4 py-2 rounded-md hover:bg-gray-200 hover:text-black hover:font-semibold">Sign Up</button>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.com?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.com →
-        </a>
-      </footer>
+      </nav>
+
+      <div className="mt-26 text-center text-white px-4">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold leading-tight">
+          Draw Ideas. <br />
+          Share Visions <br />
+          Compete Creatively
+        </h1>
+        <p className="mt-6 text-2xl text-white max-w-2xl mx-auto">
+          Join now and pretend to be an artist like the rest of us
+        </p>
+        <div className="mt-8">
+          <button 
+            className="bg-indigo-800 text-white text-lg font-semibold px-6 py-3 rounded-lg transform transition-transform duration-300 hover:bg-gray-200 hover:text-black hover:scale-105">
+            Start creating chaos
+          </button>
+        </div>
+      </div>
+
+      
+
     </div>
   );
 }
